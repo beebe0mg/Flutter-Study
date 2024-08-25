344cc10bbdbc944ca64e9f3cdaeee0d5071266f2
정상미 <w2321@e-mirim.hs.kr>
2024-08-25 22:55:49 +0900

    Learn : 스택 사용해보기

--- flutter_study/lib/main.dart
+++ flutter_study/lib/main.dart
@@ -35,39 +35,33 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text("Test Title"),
       ),
       body: Container(
-        alignment: Alignment.topLeft,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.yellow,
+        child: Stack(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width / 2,
-                  height: 200,
-                  color: Colors.blue,
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width / 2,
-                  height: 200,
-                  color: Colors.red,
-                ),
-              ],
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.red,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width / 2,
-                  height: 200,
-                  color: Colors.green,
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width / 2,
-                  height: 200,
-                  color: Colors.purple,
-                ),
-              ],
+            Container(
+              width: 200,
+              height: 200,
+              margin: const EdgeInsets.only(top: 50, left: 50),
+              color: Colors.blue,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              margin: const EdgeInsets.only(top: 100, left: 100),
+              color: Colors.green,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              margin: const EdgeInsets.only(top: 150, left: 150),
+              color: Colors.orange,
             ),
           ],
         ),
