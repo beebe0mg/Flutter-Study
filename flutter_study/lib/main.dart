87a75310e5b17afe21dc6509c58070e673379f33
정상미 <w2321@e-mirim.hs.kr>
2024-08-25 12:57:24 +0900

    Learn : 행과 열 구성하기

--- flutter_study/lib/main.dart
+++ flutter_study/lib/main.dart
@@ -34,28 +34,43 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text("Test Title"),
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const <Widget> [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text("Drawer Header Part"),
+      body: Container(
+        alignment: Alignment.topLeft,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: 200,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: 200,
+                  color: Colors.red,
+                ),
+              ],
             ),
-              ListTile(
-                title: Text("Menu 1"),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: 200,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: 200,
+                  color: Colors.purple,
+                ),
+              ],
+            ),
           ],
         ),
-      ),
-      body: const Center (
-        child: Text("HELLO WORLD")
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => print("clicked"),
-        child: const Icon(Icons.mouse),
       ),
     );
   }
